f2b7e95dfa51ed30870a625b92014fce0ac389af
Silvio Watakabe <swat2014@outlook.com>
2024-11-24 15:57:10 -0500

    - Usado o mesmo form new e feito a edição da tareda - Alterado o insert para save, trabalhando tanto insert como update

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -5,24 +5,45 @@ module.exports = class TaskController {
         res.render('tasks/new')
     }
 
-    static async insert(req, res) {
-        const title = req.body.title
-        const description = req.body.description
-        const done = false
-        const taskData = {
-            title,
-            description,
-            done
-        }
+    static async edit(req, res) {
+        const id = req.params.id
+        const task = await Task.findOne({ raw: true, where: { id: id } })
 
-        await Task.create(taskData);
-
-        res.redirect('/tasks')
+        res.render('tasks/new', { task })
     }
 
     static async index(req, res) {
         const tasks = await Task.findAll({ raw: true })
 
         res.render('tasks/index', { tasks })
+    }
+
+    static async delete(req, res) {
+        const id = req.params.id
+        await Task.destroy({ where: { id: id } })
+
+        res.redirect('/tasks')
+    }
+
+    static async save(req, res) {
+        const id = req.body.id
+        const title = req.body.title
+        const description = req.body.description
+        let done = false
+        if (req.body.done) {
+            done = true
+        }
+        const taskData = {
+            title,
+            description,
+            done
+        }
+        if (id) {
+            await Task.update(taskData, { where: { id: id } });
+        } else {
+            await Task.create(taskData);
+        }
+
+        res.redirect('/tasks')
     }
 }
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -4,7 +4,9 @@ const router = express.Router()
 const TaskController = require('../controllers/TaskController')
 
 router.get('/new', TaskController.new)
-router.post('/insert', TaskController.insert)
+router.post('/save', TaskController.save)
 router.get('/', TaskController.index)
+router.get('/edit/:id', TaskController.edit)
+router.get('/delete/:id', TaskController.delete)
 
 module.exports = router
